ea6c4f77308fa431cc73080702a1d7db5451c43d
guanbinrui <guanbinrui@dimension.im>
2019-10-08 23:37:45 +0800

    :rocket: recreate migration

--- src/migrations/1570527616458-Start.ts
+++ src/migrations/1570527616458-Start.ts
@@ -1,10 +1,10 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class startup1568886769955 implements MigrationInterface {
+export class Start1570527616458 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`CREATE TABLE "vendor" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "UQ_f61018bdc439c6d1a941261b671" UNIQUE ("name"), CONSTRAINT "PK_931a23f6231a57604f5a0e32780" PRIMARY KEY ("id"))`, undefined);
-        await queryRunner.query(`CREATE TABLE "image" ("id" SERIAL NOT NULL, "vid" bigint NOT NULL, "vendorId" integer, CONSTRAINT "PK_d6db1ab4ee9ad9dbe86c64e4cc3" PRIMARY KEY ("id"))`, undefined);
+        await queryRunner.query(`CREATE TABLE "image" ("id" SERIAL NOT NULL, "vid" character varying NOT NULL, "vendorId" integer, CONSTRAINT "UQ_5ed10fd6122688945554c23b093" UNIQUE ("vid"), CONSTRAINT "PK_d6db1ab4ee9ad9dbe86c64e4cc3" PRIMARY KEY ("id"))`, undefined);
         await queryRunner.query(`ALTER TABLE "image" ADD CONSTRAINT "FK_3a8a1ed0ec3e3c176c5225456eb" FOREIGN KEY ("vendorId") REFERENCES "vendor"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`, undefined);
     }
 
